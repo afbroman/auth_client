7d5401758cdcef1d1432c39f1b9bc98dfe1311de
Andrew Broman <broman@acm.org>
2016-05-06 22:19:05 -0500

    Add redirect to feature on successful auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
     // Submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // If request is good:
-    // - update state to indicate user is authenticated
-    // - save the JWT token
-    // - redirect to the route '/feature'
-
-    // If request is bad:
-    // - show an error to the user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good:
+        // - update state to indicate user is authenticated
+        // - save the JWT token
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad:
+        // - show an error to the user
+      });
   }
 }
