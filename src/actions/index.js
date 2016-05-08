ceccc2314d0314d0a13e69406aad8b722e9a8bd9
Andrew Broman <broman@acm.org>
2016-05-07 21:31:20 -0500

    Save token to local storage

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,7 @@ export function signinUser({ email, password }) {
         // - update state to indicate user is authenticated
         dispatch({ type: AUTH_USER });
         // - save the JWT token
+        localStorage.setItem('token', response.data.token);
         // - redirect to the route '/feature'
         browserHistory.push('/feature');
       })
