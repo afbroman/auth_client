02dba680bb743ab8ce46814e75eda8ccf59e70bb
Andrew Broman <broman@acm.org>
2016-05-10 21:14:51 -0500

    Add form logic

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,20 +4,29 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   render() {
-    const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+    const { handleSubmit, fields: { email, password, passwordConfirm } } = this.props;
     return (
       <form>
         <fieldset className="form-group">
           <label>Email:</label>
           <input {...email} className="form-control" />
+          {email.touched &&
+            email.error &&
+            <div className="error text-danger">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
           <input {...password} type="password" className="form-control" />
+          {password.touched &&
+            password.error &&
+            <div className="error text-danger">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input {...passwordConfirm} type="password" className="form-control" />
+          {passwordConfirm.touched &&
+            passwordConfirm.error &&
+            <div className="error text-danger">{passwordConfirm.error}</div>}
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign up</button>
       </form>
@@ -25,7 +34,30 @@ class Signup extends Component {
   }
 }
 
+function validate(formProps) {
+  const errors = {};
+
+  if (!formProps.email) {
+    errors.email = 'Please enter an email';
+  }
+
+  if (!formProps.password) {
+    errors.password = 'Please enter a password';
+  }
+
+  if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'Please enter a password confirmation';
+  }
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate,
 })(Signup);
