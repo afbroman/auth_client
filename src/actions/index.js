5da306b21d9aa96d02485978c86bab398a18909a
Andrew Broman <broman@acm.org>
2016-05-12 18:18:49 -0500

    Finish implementation of signup

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,20 @@ import { browserHistory } from 'react-router';
 import {
   AUTH_USER,
   UNAUTH_USER,
-  AUTH_ERROR
+  AUTH_ERROR,
 } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error,
+  };
+}
+
 export function signinUser({ email, password }) {
-  return function(dispatch) {
+  return function (dispatch) {
     // Submit email/password to server
     axios.post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
@@ -26,21 +33,23 @@ export function signinUser({ email, password }) {
         // - show an error to the user
         dispatch(authError('Bad Login Info'));
       });
-  }
+  };
 }
 
 export function signupUser({ email, password }) {
-  return function(dispatch) {
+  return function (dispatch) {
     axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(response => {
+        dispatch(authError(response.data.error));
+      });
   };
 }
 
-export function authError(error) {
-  return {
-    type: AUTH_ERROR,
-    payload: error
-  };
-}
 
 export function signoutUser() {
   localStorage.removeItem('token');
